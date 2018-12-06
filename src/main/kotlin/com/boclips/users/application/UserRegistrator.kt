064080a4ec5b9d4f9e666ecf68a223438432969f
Matt Jones <matt@boclips.com>
2018-12-06 14:50:23 +0000

    Add logging
    Signed-off-by: José Carlos Valero Sánchez <josecarlos@boclips.com>

--- src/main/kotlin/com/boclips/users/application/UserRegistrator.kt
+++ src/main/kotlin/com/boclips/users/application/UserRegistrator.kt
@@ -2,6 +2,7 @@ package com.boclips.users.application
 
 import com.boclips.users.domain.service.UserService
 import com.boclips.users.domain.model.users.IdentityProvider
+import mu.KLogging
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalDate
@@ -11,10 +12,12 @@ class UserRegistrator(
         val identityProvider: IdentityProvider,
         val userService: UserService
 ) {
+    companion object : KLogging()
 
     @Scheduled(fixedDelayString = "\${app.registration-period-in-seconds}")
     fun registerNewTeachersSinceYesterday() = identityProvider
             .getLastLoginUserIds("educators", LocalDate.now().minusDays(1))
+            .apply { logger.info { "Found ${this.size} login events in the past 24 hours - checking registration state" } }
             .forEach { userService.registerUserIfNew(it) }
 
 }
--- src/main/kotlin/com/boclips/users/domain/service/UserService.kt
+++ src/main/kotlin/com/boclips/users/domain/service/UserService.kt
@@ -5,6 +5,7 @@ import com.boclips.users.domain.model.users.UserRepository
 import com.boclips.users.domain.model.events.AnalyticsClient
 import com.boclips.users.domain.model.events.Event
 import com.boclips.users.domain.model.events.EventType
+import mu.KLogging
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,12 +14,18 @@ class UserService(
         val userRepository: UserRepository,
         val analyticsClient: AnalyticsClient
 ) {
+
+    companion object : KLogging()
+
     @Synchronized // this should be transactional to avoid multi-instance race conditions. Not critical though.
     fun registerUserIfNew(id: String): User =
             userRepository.findById(id)
             ?: userRepository
                     .save(User(id = id, activated = false))
-                    .apply { analyticsClient.track(Event(eventType = EventType.BEGIN_ACTIVATION, userId = id)) }
+                    .apply {
+                        analyticsClient.track(Event(eventType = EventType.BEGIN_ACTIVATION, userId = id))
+                        logger.info { "Registered new user: $id" }
+                    }
     fun activate(id: String) = userRepository.save(User(id = id, activated = true))
     fun findById(id: String) = userRepository.findById(id)
 }
