a44dbe8a9d14bc084b65fd4b312518636f76f87a
Thea Kramer <thea@boclips.com>
2019-02-22 16:23:53 +0000

    Revert "Look for teachers within the last 7 days"
    This reverts commit d8bc31f371c67d13f9dba873a9e68879f8c6a0af.

--- src/main/kotlin/com/boclips/users/application/UserRegistrator.kt
+++ src/main/kotlin/com/boclips/users/application/UserRegistrator.kt
@@ -16,7 +16,7 @@ class UserRegistrator(
 
     @Scheduled(fixedDelayString = "\${app.registration-period-in-millis}")
     fun registerNewTeachersSinceYesterday() = identityProvider
-        .getNewTeachers(LocalDate.now().minusDays(7))
+        .getNewTeachers(LocalDate.now().minusDays(1))
         .apply { logger.info { "Found ${this.size} login events in the past 24 hours - checking registration state" } }
         .forEach { userService.registerUserIfNew(it.id) }
 }
