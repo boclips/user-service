49e2ec6adcb9a47980c5d3c01a20968b98676885
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2019-03-25 22:15:55 +0000

    Delete unused file
    [#164657584]

--- src/main/kotlin/com/boclips/users/domain/model/AccountMetadata.kt
+++
@@ -1,5 +0,0 @@
-package com.boclips.users.domain.model
-
-import com.boclips.users.domain.model.analytics.AnalyticsId
-
-data class AccountMetadata(val subjects: String?, val analyticsId: AnalyticsId?)
