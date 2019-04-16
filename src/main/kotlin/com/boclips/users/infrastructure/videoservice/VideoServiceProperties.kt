ee8839f4185d5c28ccaadb2947708d4d081c4edf
Matt Jones <matt@boclips.com>
2019-04-16 14:50:08 +0100

    Rename videoservice in configuration properties
    [#164780959]

--- src/main/kotlin/com/boclips/users/infrastructure/videoservice/VideoServiceProperties.kt
+++ src/main/kotlin/com/boclips/users/infrastructure/videoservice/VideoServiceProperties.kt
@@ -3,7 +3,7 @@ package com.boclips.users.infrastructure.videoservice
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 
-@ConfigurationProperties("video-service")
+@ConfigurationProperties("videoservice")
 @Component
 class VideoServiceProperties {
     lateinit var baseUrl: String
