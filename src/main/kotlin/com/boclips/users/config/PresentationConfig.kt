dd03e7e45a20b673565d930dad9deebd562e4bf0
Jacek Rzeniewicz <jacek@boclips.com>
2019-05-07 16:45:58 +0200

    Upgrade Spring Boot to 2.1.3

---
+++ src/main/kotlin/com/boclips/users/config/PresentationConfig.kt
@@ -0,0 +1,17 @@
+package com.boclips.users.config
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.ForwardedHeaderFilter
+
+@Configuration
+class PresentationConfig {
+
+    @Bean
+    fun forwardedHeaderFilter(): FilterRegistrationBean<ForwardedHeaderFilter> {
+        val filter = FilterRegistrationBean<ForwardedHeaderFilter>()
+        filter.filter = ForwardedHeaderFilter()
+        return filter
+    }
+}
