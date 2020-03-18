42b746e665afd453816fc9bb20b75e3c4f949931
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2020-03-18 06:54:51 +0000

    Add logging to user association

--- src/main/kotlin/com/boclips/users/application/commands/AssignUsersByOrganisationDomain.kt
+++ src/main/kotlin/com/boclips/users/application/commands/AssignUsersByOrganisationDomain.kt
@@ -6,6 +6,7 @@ import com.boclips.users.domain.model.organisation.OrganisationId
 import com.boclips.users.domain.service.OrganisationRepository
 import com.boclips.users.domain.service.UserRepository
 import com.boclips.users.domain.service.UserUpdateCommand
+import mu.KLogging
 import org.springframework.stereotype.Component
 
 @Component
@@ -13,12 +14,16 @@ class AssignUsersByOrganisationDomain(
     private val organisationRepository: OrganisationRepository,
     private val userRepository: UserRepository
 ) {
+    companion object : KLogging()
+
     operator fun invoke(id: String): List<User> {
         val organisation =
             organisationRepository.findOrganisationById(OrganisationId(id)) ?: throw OrganisationNotFoundException(id)
         val domain = organisation.details.domain
 
         val orphanUsers = domain?.let { userRepository.findOrphans(it, organisation.id) } ?: emptyList()
+
+        logger.info { "Identified ${orphanUsers.size} users not associate to organisation ${organisation.id} with domain $domain" }
 
         return orphanUsers.map { userRepository.update(it, UserUpdateCommand.ReplaceOrganisationId(organisation.id)) }
     }
