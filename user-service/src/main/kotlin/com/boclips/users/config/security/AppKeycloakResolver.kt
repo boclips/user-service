e38f7a6f5652af0bbe3b5ce823d820ebad722ba0
Andrew Bruce <me@andrewbruce.net>
2020-05-13 14:36:39 +0100

    Replace keycloak-*.json with properties
    This lets us configure arbitrary environments, and to change config at
    deploy time.
    
    https://12factor.net/config
    
    [#172381472]
    
    Signed-off-by: Matt Jones <matt@boclips.com>

---
+++ user-service/src/main/kotlin/com/boclips/users/config/security/AppKeycloakResolver.kt
@@ -0,0 +1,24 @@
+package com.boclips.users.config.security
+
+import com.boclips.users.infrastructure.keycloak.KeycloakProperties
+import org.keycloak.adapters.KeycloakConfigResolver
+import org.keycloak.adapters.KeycloakDeployment
+import org.keycloak.adapters.spi.HttpFacade
+import org.keycloak.common.enums.SslRequired
+import org.keycloak.representations.adapters.config.AdapterConfig
+import org.springframework.stereotype.Component
+
+@Component
+class AppKeycloakConfigResolver(private val keycloakProperties: KeycloakProperties) : KeycloakConfigResolver {
+    override fun resolve(facade: HttpFacade.Request?): KeycloakDeployment =
+        KeycloakDeployment().apply {
+            isBearerOnly = true
+            sslRequired = SslRequired.EXTERNAL
+            confidentialPort = 0
+            isUseResourceRoleMappings = true
+
+            resourceName = "user-service"
+            realm = keycloakProperties.realm
+            setAuthServerBaseUrl(AdapterConfig().apply { authServerUrl = keycloakProperties.url })
+        }
+}
