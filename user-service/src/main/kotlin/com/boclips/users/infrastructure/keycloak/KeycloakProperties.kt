5803b1b319b7a214587d5fafcb3e04b7fe9c4878
Benjamin Grohbiel <benjamin.groehbiel@gmail.com>
2020-05-15 10:55:40 +0100

    Revert "Replace keycloak-*.json with properties"
    This reverts commit e38f7a6f5652af0bbe3b5ce823d820ebad722ba0.

--- user-service/src/main/kotlin/com/boclips/users/infrastructure/keycloak/KeycloakProperties.kt
+++ user-service/src/main/kotlin/com/boclips/users/infrastructure/keycloak/KeycloakProperties.kt
@@ -2,15 +2,11 @@ package com.boclips.users.infrastructure.keycloak
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
-import org.springframework.validation.annotation.Validated
-import javax.validation.constraints.NotBlank
 
 @ConfigurationProperties("keycloak")
-@Validated
 @Component
 class KeycloakProperties {
-    @NotBlank lateinit var realm: String
-    @NotBlank lateinit var url: String
-    @NotBlank lateinit var username: String
-    @NotBlank lateinit var password: String
+    lateinit var url: String
+    lateinit var username: String
+    lateinit var password: String
 }
