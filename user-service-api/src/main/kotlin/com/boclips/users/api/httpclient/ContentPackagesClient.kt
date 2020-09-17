e43c99a7bbfe19e72c54f00a1c2f7281d0143551
Sebastian Kazenbroot-Guppy <sebastian@boclips.com>
2020-09-17 14:41:41 +0100

    Added content packages client
    [#173809755]

---
+++ user-service-api/src/main/kotlin/com/boclips/users/api/httpclient/ContentPackagesClient.kt
@@ -0,0 +1,42 @@
+package com.boclips.users.api.httpclient
+
+import com.boclips.users.api.httpclient.helper.ObjectMapperDefinition
+import com.boclips.users.api.httpclient.helper.TokenFactory
+import com.boclips.users.api.response.accessrule.ContentPackageResource
+import com.fasterxml.jackson.databind.ObjectMapper
+import feign.Feign
+import feign.Logger
+import feign.Param
+import feign.RequestLine
+import feign.RequestTemplate
+import feign.jackson.JacksonDecoder
+import feign.jackson.JacksonEncoder
+import feign.okhttp.OkHttpClient
+import feign.slf4j.Slf4jLogger
+
+interface ContentPackagesClient {
+    @RequestLine("GET /v1/content-packages/{id}")
+    fun find(@Param("id") id: String): ContentPackageResource
+
+    companion object {
+        @JvmStatic
+        fun create(
+            apiUrl: String,
+            objectMapper: ObjectMapper = ObjectMapperDefinition.default(),
+            tokenFactory: TokenFactory? = null
+        ): ContentPackagesClient {
+            return Feign.builder()
+                .client(OkHttpClient())
+                .encoder(JacksonEncoder(objectMapper))
+                .decoder(JacksonDecoder(objectMapper))
+                .requestInterceptor { template: RequestTemplate ->
+                    if (tokenFactory != null) {
+                        template.header("Authorization", "Bearer ${tokenFactory.getAccessToken()}")
+                    }
+                }
+                .logLevel(Logger.Level.BASIC)
+                .logger(Slf4jLogger())
+                .target(ContentPackagesClient::class.java, apiUrl)
+        }
+    }
+}
--- user-service/src/test/kotlin/com/boclips/users/UserServiceClientE2ETest.kt
+++ user-service/src/test/kotlin/com/boclips/users/UserServiceClientE2ETest.kt
@@ -1,5 +1,6 @@
 package  com.boclips.users
 
+import com.boclips.users.api.httpclient.ContentPackagesClient
 import com.boclips.users.api.httpclient.OrganisationsClient
 import com.boclips.users.api.httpclient.UsersClient
 import com.boclips.users.api.httpclient.helper.ObjectMapperDefinition
@@ -15,6 +16,7 @@ import com.boclips.users.testsupport.factories.TeacherPlatformAttributesFactory
 import com.boclips.users.testsupport.factories.UserFactory
 import feign.FeignException
 import org.assertj.core.api.Assertions.assertThat
+import org.bson.types.ObjectId
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -128,6 +130,40 @@ class UserServiceClientE2ETest : AbstractSpringIntegrationTest() {
 
             assertThat(organisationResource.id).isEqualTo(organisation.id.value)
             assertThat(organisationResource.organisationDetails.name).isEqualTo("hello")
+        }
+    }
+
+    @Nested
+    inner class ContentPackages {
+        lateinit var contentPackagesClient: ContentPackagesClient
+
+        @BeforeEach
+        fun setUp() {
+            contentPackagesClient = ContentPackagesClient.create(
+                apiUrl = "http://localhost:$randomServerPort",
+                objectMapper = ObjectMapperDefinition.default(),
+                tokenFactory = TestTokenFactory(
+                    "the@owner.com",
+                    UserRoles.VIEW_CONTENT_PACKAGES
+                )
+            )
+        }
+
+        @Test
+        fun `can fetch a content package`() {
+            val contentPackage = saveContentPackage(
+                ContentPackageFactory.sample(
+                    name = "content package",
+                    accessRules = listOf(
+                        AccessRuleFactory.sampleIncludedVideosAccessRule()
+                    )
+                )
+            )
+            val resource = contentPackagesClient.find(contentPackage.id.value)
+
+            assertThat(resource.id).isEqualTo(contentPackage.id.value)
+            assertThat(resource.name).isEqualTo("content package")
+            assertThat(resource.accessRules).hasSize(1)
         }
     }
 }
