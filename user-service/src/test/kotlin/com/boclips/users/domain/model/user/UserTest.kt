43b0ccb72822538bb5e171a4d51269f6eb3072a1
Michal Wit <michal@boclips.com>
2020-09-21 10:49:00 +0200

    Add two learning tests for computing access expiration date
    [#174876563]

--- user-service/src/test/kotlin/com/boclips/users/domain/model/user/UserTest.kt
+++ user-service/src/test/kotlin/com/boclips/users/domain/model/user/UserTest.kt
@@ -1,23 +1,33 @@
 package com.boclips.users.domain.model.user
 
 import com.boclips.users.domain.model.feature.Feature
+import com.boclips.users.domain.model.organisation.Deal
 import com.boclips.users.testsupport.factories.OrganisationFactory
 import com.boclips.users.testsupport.factories.UserFactory
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
+import java.time.ZonedDateTime
 
 class UserTest {
 
     @Test
     fun `returns features assigned to organisation`() {
         // given
-        val user = UserFactory.sample(organisation = OrganisationFactory.school(features = null))
+        val user = UserFactory.sample(organisation = OrganisationFactory.school(
+                features = mapOf(Feature.TEACHERS_HOME_BANNER to false)
+        ))
 
         // when
         val features = user.features
 
         // then
-        assertThat(features).isEqualTo(Feature.DEFAULT_VALUES)
+        assertThat(features).isEqualTo(mapOf(
+                Feature.LTI_COPY_RESOURCE_LINK to false,
+                Feature.TEACHERS_HOME_BANNER to false,
+                Feature.TEACHERS_HOME_SUGGESTED_VIDEOS to true,
+                Feature.TEACHERS_HOME_PROMOTED_COLLECTIONS to true,
+                Feature.TEACHERS_SUBJECTS to true
+        ))
     }
 
     @Test
@@ -42,5 +52,57 @@ class UserTest {
 
         // then
         assertThat(features).isEqualTo(Feature.DEFAULT_VALUES)
+    }
+
+    @Test
+    fun `resolves explicitly assigned access expiration date`() {
+        // given
+        val user = UserFactory.sample(accessExpiresOn = REFERENCE_NOW)
+
+        // when
+        val accessExpiresOn = user.accessExpiresOn
+
+        // then
+        assertThat(accessExpiresOn).isEqualTo(REFERENCE_NOW)
+    }
+
+    @Test
+    fun `organisation expiration date takes precedence over the date explicitly assigned to a user`() {
+        // given
+        val user = UserFactory.sample(
+                accessExpiresOn = YESTERDAY,
+                organisation = OrganisationFactory.district(
+                    deal = Deal(billing = true, accessExpiresOn = REFERENCE_NOW)
+                )
+        )
+
+        // when
+        val accessExpiresOn = user.accessExpiresOn
+
+        // then
+        assertThat(accessExpiresOn).isEqualTo(REFERENCE_NOW)
+    }
+
+    @Test
+    fun `returns null when expiration date is set neither for the user nor for the organisation`() {
+        // given
+        val user = UserFactory.sample(
+                accessExpiresOn = null,
+                organisation = OrganisationFactory.district(
+                    deal = Deal(billing = true, accessExpiresOn = null)
+                )
+        )
+
+        // when
+        val accessExpiresOn = user.accessExpiresOn
+
+        // then
+        assertThat(accessExpiresOn).isNull()
+    }
+
+    private companion object {
+
+        val REFERENCE_NOW: ZonedDateTime = ZonedDateTime.now()
+        val YESTERDAY: ZonedDateTime = REFERENCE_NOW.minusDays(1)
     }
 }
