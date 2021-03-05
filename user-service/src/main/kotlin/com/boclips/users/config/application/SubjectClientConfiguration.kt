b9c9b2e7e5c1f250c4a77ae2dee7dd01dbb40bda
Michal Wit <michal@boclips.com>
2021-03-05 08:32:06 +0100

    Trace user-service -> video-service HTTP communication
    [#176322538]
    
    Signed-off-by: Mima Farley-Rose <mima@boclips.com>

--- user-service/src/main/kotlin/com/boclips/users/config/application/SubjectClientConfiguration.kt
+++ user-service/src/main/kotlin/com/boclips/users/config/application/SubjectClientConfiguration.kt
@@ -2,16 +2,23 @@ package com.boclips.users.config.application
 
 import com.boclips.users.infrastructure.videoservice.VideoServiceProperties
 import com.boclips.videos.api.httpclient.SubjectsClient
+import feign.opentracing.TracingClient
+import io.opentracing.Tracer
+import feign.okhttp.OkHttpClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
 class SubjectClientConfiguration(
-    private val videoServiceProperties: VideoServiceProperties
+    private val videoServiceProperties: VideoServiceProperties,
+    private val tracer: Tracer
 ) {
 
     @Profile("!test")
     @Bean
-    fun subjectsClient() = SubjectsClient.create(apiUrl = videoServiceProperties.baseUrl)
+    fun subjectsClient() = SubjectsClient.create(
+        apiUrl = videoServiceProperties.baseUrl,
+        feignClient = TracingClient(OkHttpClient(), tracer)
+    )
 }
